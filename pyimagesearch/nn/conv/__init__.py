e2b2986b972c6804aeb57423da3c4810027ca679
Duhai Alshukaili <duhai.alshukaili@gmail.com>
2025-06-03 10:33:32 +0400

    Some house keeping and added VGGNet5

--- pyimagesearch/nn/conv/__init__.py
+++ pyimagesearch/nn/conv/__init__.py
@@ -1,3 +1,4 @@
 from .shallownet import ShallowNet
 from .lenet import LeNet
-from .minivggent import MiniVGGNet
+from .minivggnet import MiniVGGNet
+from .vggnet5 import VGGNet5
---
+++ pyimagesearch/nn/conv/vggnet5.py
@@ -0,0 +1,70 @@
+# import the ncessary packages
+import keras
+from keras import layers
+from keras import backend as K
+
+
+import tensorflow as tf
+
+
+class VGGNet5:
+
+    @staticmethod
+    def build(width, height, depth, classes):
+        model = keras.Sequential()
+        inputShape = (height, width, depth)
+        chanDim = -1
+
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
+            chanDim = 1
+        
+
+        # Input Layer
+        model.add(layers.Input(shape=inputShape))
+
+        # number of filter in each of the CONV => RELU => CONV => RELU => POOL layer set
+        filters = [32 * (2 ** i) for i in range(5)]
+
+        for  size in filters:
+            model.add(layers.Conv2D(size, (3,3), padding="same"))
+            model.add(layers.Activation("relu"))
+            model.add(layers.BatchNormalization(axis=chanDim))
+            model.add(layers.Conv2D(size, (3,3), padding="same"))
+            model.add(layers.Activation("relu"))
+            model.add(layers.BatchNormalization(axis=chanDim))
+            model.add(layers.MaxPooling2D(pool_size=(2,2)))
+            model.add(layers.Dropout(0.25))
+
+        # first CONV => RELU => CONV => RELU => POOL layer set
+        # model.add(layers.Conv2D(32, (3,3), padding="same"))
+        # model.add(layers.Activation("relu"))
+        # model.add(layers.BatchNormalization(axis=chanDim))
+        # model.add(layers.Conv2D(32, (3,3), padding="same"))
+        # model.add(layers.Activation("relu"))
+        # model.add(layers.BatchNormalization(axis=chanDim))
+        # model.add(layers.MaxPooling2D(pool_size=(2,2)))
+        # model.add(layers.Dropout(0.25))
+
+        # second CONV => RELU => CONV => RELU => POOL layer set
+        # model.add(layers.Conv2D(64, (3,3), padding="same"))
+        # model.add(layers.Activation("relu"))
+        # model.add(layers. BatchNormalization(axis=chanDim))
+        # model.add(layers.Conv2D(64, (3,3), padding="same"))
+        # model.add(layers.Activation("relu"))
+        # model.add(layers. BatchNormalization(axis=chanDim))
+        # model.add(layers.MaxPooling2D(pool_size=(2,2)))
+        # model.add(layers.Dropout(0.25))
+
+        # first and only set of FC => RELU 
+        model.add(layers.Flatten())
+        model.add(layers.Dense(512))
+        model.add(layers.Activation("relu"))
+        model.add(layers.BatchNormalization(axis=chanDim))
+        model.add(layers.Dropout(0.5))
+
+        # softmax classifier
+        model.add(layers.Dense(classes))
+        model.add(layers.Activation("softmax"))
+
+        return model
