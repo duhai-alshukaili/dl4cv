1503050f0da6f130c8704004deafb455b3c44923
Duhai Alshukaili <duhai.alshukaili@gmail.com>
2025-06-03 10:11:19 +0400

    Modifed the optimizer in minivggnet_cifar10

--- ch15/minivggnet_cifar10.py
+++ ch15/minivggnet_cifar10.py
@@ -44,7 +44,7 @@ labelNames = ["airplane", "automobile", "bird", "cat", "deer",
 
 # initialize the optimizer and model
 print("[INFO] compiling model ...")
-opt = optimizers.SGD(learning_rate=0.01)
+opt = optimizers.SGD(learning_rate=0.01, weight_decay=0.01/40, momentum=0.9, nesterov=True)
 model = MiniVGGNet.build(width=32, height=32, depth=3, classes=10)
 model.compile(loss="categorical_crossentropy", optimizer=opt, 
     metrics=["accuracy"])
